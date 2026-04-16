46cc97cfd149fccc6240ff09a8267437cbcf1a19
Huynguyencode835 <142970556+Huynguyencode835@users.noreply.github.com>
2026-04-16 18:28:52 +0700

    Shedule

---
+++ Fontend/myproject/utils/Apis.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const Base_URL = 'http://192.168.100.110:8000/';
+
+export const endpoints = {
+    'doctors': '/doctors/',
+    'doctorDetail': (id) => `/doctors/${id}/doctor_detail/`
+}
+
+export default axios.create({
+    baseURL: Base_URL
+});
